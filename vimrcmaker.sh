3ea37b5030ece60818caa1632252ed0db3dbddf1
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2022-11-19 20:37:34 +0100

    Add files via upload

--- vimrcmaker.sh
+++ vimrcmaker.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-vimfilelocation="/home/$(whoami)/.vimrc"
+VIMFILELOCATION=/home/$(whoami)/.vimrc
+PATHTOSWAPS=/home/$(whoami)/.cache/vim/swap
+RCEXISTSVAL=-1
 
-rcexistsval=-1
+
+
+
 rcfileisexists () {
-	if [ -f "$vimfilelocation" ]; then
-		rcexistsval=1
+	if [ -f "$VIMFILELOCATION" ]; then
+		RCEXISTSVAL=1
 	else
-		rcexistsval=0
+		RCEXISTSVAL=0
 	fi
 }
 rcfileisexists
-if [ $rcexistsval -eq 0 ]; then
-	touch "$vimfilelocation"
+if [ $RCEXISTSVAL -eq 0 ]; then
+	touch "$VIMFILELOCATION"
 	echo "call plug#begin()
 	Plug 'jacoborus/tender.vim'
 	Plug 'rafi/awesome-vim-colorschemes'
@@ -31,9 +35,9 @@ call plug#end()
 	set tabstop=4
 	set shiftwidth=4
 	set softtabstop=4
-	set expandtab" >> "$vimfilelocation"
+	set expandtab" >> "$VIMFILELOCATION"
 	echo "Vimfile is created successfully!"
-elif [ $rcexistsval -eq 1 ]; then
+elif [ $RCEXISTSVAL -eq 1 ]; then
 	echo "Vimfile is already exist! Do you want to remove(1) or rename(2) it?"
 	read -p "Inputs: '1' for remove, '2' for rename: " vimfile
 
@@ -43,12 +47,27 @@ elif [ $rcexistsval -eq 1 ]; then
 	done
 
 	if [ "$vimfile" = "1" ]; then
-		rm ${vimfilelocation}
+		rm ${VIMFILELOCATION}
 		echo "Vimfile has been removed!"
 	elif [ "$vimfile" = "2" ]; then
 		echo "Type the new name of the .vimrc!"
 		read newname
-		mv ${vimfilelocation} ~/${newname}
+		mv ${VIMFILELOCATION} ~/${newname}
 		echo "The vimfile's name has been changed to '${newname}'!"
 	fi
+fi
+
+if [ ! -d "$PATHTOSWAPS" ]; then
+    echo "'${PATHTOSWAPS}' does not exist! Do you want to create one? [y/N]"
+		read create
+		if [ -z "$create" ]; then
+			create="N"
+		fi
+		if [ "${create^}" = "Y" ]; then
+			mkdir -p $PATHTOSWAPS
+		elif [ "${create^}" = "N" ]; then
+			exit
+		else
+			echo "Correct inputs: 'y or Y' =yes, 'n or N' =no!"
+		fi
 fi
